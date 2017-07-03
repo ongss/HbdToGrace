d44ada936eeed90a95082a7a3c5c036a43b91d5c
ongss <ong54321@hotmail.com>
2017-07-03 22:42:55 +0700

    add gace face and body

--- code/main.js
+++ code/main.js
@@ -1,8 +1,17 @@
 window.onload = function(){
 	
 	//var
-	var img = document.getElementById('test');
+
+	//Resource
 	var bg = document.getElementById('bg');
+	var body1 = document.getElementById('body1');
+	var body2 = document.getElementById('body2');
+	var body3 = document.getElementById('body3');
+	var body4 = document.getElementById('body4');
+	var eat1 = document.getElementById('eat1');
+	var eat2 = document.getElementById('eat2');
+	var normal = document.getElementById('normal');
+	var openMouse = document.getElementById('openMouse');
 
 	var main = document.getElementById('main');
 	var mainctx = main.getContext('2d');
@@ -11,7 +20,7 @@ window.onload = function(){
 
 	var status = document.getElementById('status');
 
-	var Grace = new grace(400,1,true);
+	var Grace = new grace(400,1,'eatting');
 
 	var game = document.getElementById('game');
 
@@ -20,6 +29,9 @@ window.onload = function(){
 		this.x = x;
 		this.size = size;
 		this.eatState = eatState;
+		this.eatcnt = 0;
+		this.face = null;
+
 		this.leftMove = function(){
 			if(this.x>=0){
 				this.x-=7;
@@ -30,8 +42,45 @@ window.onload = function(){
 				this.x+=7;
 			}
 		}
-		this.draw = function(){;
-			mainctx.drawImage(img,this.x,280,103.3,186.6);
+		this.draw = function(){
+
+			//body
+			if(this.size === 1){
+				this.body = body1;
+			}
+			else if(this.size === 2){
+				this.body = body2;
+			}
+			else if(this.size === 3){
+				this.body = body3;
+			}
+			else if(this.size === 4){
+				this.body = body4;
+			}
+
+
+			//face
+			if(this.eatState === 'eatting' && Math.floor(this.eatcnt/25)%2 === 0){
+				this.face = eat1;
+				this.eatcnt+=1;
+			}
+			else if(this.eatState === 'eatting' && Math.floor(this.eatcnt/25)%2 === 1){
+				this.face = eat2;
+				this.eatcnt+=1;
+			}
+			else if(this.eatState === 'near'){
+				this.face = openMouse;
+			}
+			else if(this.eatState === 'normal'){
+				this.face = normal;
+			}
+			if(this.eatcnt >= 250){
+				this.eatcnt = 0;
+				this.eatState = 'normal';
+			}
+
+			mainctx.drawImage(this.face,this.x,280,103.3,186.6);
+			mainctx.drawImage(this.body,this.x,280,103.3,186.6);
 			//mainctx.rect(this.x,280,103.3,186.6);
 			//mainctx.stroke();
 		}
