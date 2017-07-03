24df1be7acba1389d79911165b4df0192ba0641b
ongss <ong54321@hotmail.com>
2017-07-03 21:23:45 +0700

    add status img

--- code/main.js
+++ code/main.js
@@ -9,7 +9,11 @@ window.onload = function(){
 	main.width = 768;
 	main.height = 480;
 
+	var status = document.getElementById('status');
+
 	var Grace = new grace(400,1,true);
+
+	var game = document.getElementById('game');
 
 	//class
 	function grace(x,size,eat){
@@ -39,7 +43,40 @@ window.onload = function(){
 		requestAnimationFrame(mainDisplay);
 	}
 
+	function statusimg(lv){
+		if(lv === 1){
+			var img = new Image();
+			img.src = "./resource/pic/weight_lev1.png";	
+		}
+		if(lv === 2){
+			var img = new Image();
+			img.src = "./resource/pic/weight_lev2.png";	
+		}
+		if(lv === 3){
+			var img = new Image();
+			img.src = "./resource/pic/weight_lev3.png";	
+		}
+		if(lv === 4){
+			var img = new Image();
+			img.src = "./resource/pic/weight_lev4.png";	
+		}
+		if(lv === 5){
+			var img = new Image();
+			img.src = "./resource/pic/weight_lev5.png";	
+		}
+		img.id = "status";
+		img.style.width = '204px';	
+		img.style.height = '100px';
+		game.insertBefore(img,game.firstChild);
+	}
+
+	statusimg(1);
+
 	mainDisplay();
+
+
+
+	//test
 
 	//input
 	document.addEventListener('keydown',function(event){
